11a7438f3f58a71d528ef0e4c531cf2ed0ab4c60
Sk0vsted <as.kristiansen10@hotmail.com>
2025-04-29 11:01:24 +0200

    Initilized Vue + Vite

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/hjv_logo.ico" class="logo" alt="Vite logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Militeam" />
+</template>
