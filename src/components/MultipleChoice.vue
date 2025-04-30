90415ed782584eb4b3d29d5fb7931990cbbb7467
JohnnieTorp <johnnietorp@gmail.com>
2025-04-30 12:46:30 +0200

    MultipleChoice.vue komponent
    Tilføjet MultipleChoice.vue komponent med HTML-struktur og props

---
+++ src/components/MultipleChoice.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="question-container">
+      <h2>Multiple Choice Spørgsmål</h2>
+  
+      <p><strong>Kategori:</strong> {{ category }}</p>
+      <p><strong>Spørgsmål:</strong> {{ question }}</p>
+      
+      <div class="multiple-choice-options">
+        <label v-for="(option, index) in options" :key="index">
+            <input type="radio" :name="'answer'" :value="option" />
+            {{ option }}
+        </label>
+      </div>
+    </div>
+  </template>
+
+<script>
+export default {
+  name: 'MultipleChoice',
+  props: {
+    category: String,
+    question: String,
+    options: Array
+  }
+};
+</script>
